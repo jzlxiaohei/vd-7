e547c2c63f036e0c6ec87b6831e2d94dfbc8132a
zilong <leonard.jiang@liulishuo.com>
2017-12-10 13:15:48 +0800

    feat(link): preventDefault click action

--- src/widgets/Link/Preview.jsx
+++ src/widgets/Link/Preview.jsx
@@ -14,6 +14,13 @@ class ContainerPreview extends React.Component {
     otherProps: PropTypes.object.isRequired, // className, id, style, onClick等事件
   }
 
+
+  // TODO： how to fix so ugly code
+  handleClick = (e) => {
+    e.preventDefault();
+    if(this.props.otherProps.onClick) this.props.otherProps.onClick(e);
+  }
+
   render() {
     const props = this.props;
     const attr = props.attr;
@@ -21,6 +28,7 @@ class ContainerPreview extends React.Component {
       <a
         {...props.otherProps}
         href={attr.link}
+        onClick={this.handleClick}
       >
         {attr.text || props.childNodes}
       </a>
