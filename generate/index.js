4fae98036545bc8db170d36d2fe021d1ae2541c1
zilong <leonard.jiang@liulishuo.com>
2017-12-30 20:39:14 +0800

    chore(code): remove unused package and some unused code

--- generate/index.js
+++ generate/index.js
@@ -3,9 +3,6 @@ global.__IS_DESIGN__ = false;
 const babelConfig = require('./babel-config');
 
 
-
 require('babel-core/register')(babelConfig);
-// require('babel-polyfill');
 require('./require-hook');
-// require('./rollup');
 require('./genCode');
